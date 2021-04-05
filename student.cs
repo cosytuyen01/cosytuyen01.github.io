1a78a536dc06301e31ab08e57f54bc9397b52847
cosytuyen01 <71358400+cosytuyen01@users.noreply.github.com>
2021-04-05 16:48:03 +0700

    Update student.cs

--- student.cs
+++ student.cs
@@ -1,1 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace AppOOP
+{
+    class student
+    {
+        get
+            {
+            return name;
+            }
+            set
+            {
+     		name = value;
+     	    }
+            Console.Write("Student");
+            }
+       public virtual void Talk()
+           {
+    MessageBox.Show(Name);
+           }
+    }
